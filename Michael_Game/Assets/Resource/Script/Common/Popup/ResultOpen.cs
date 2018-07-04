d009670c46f36fd00a9edd56d37345965973090f
Ryo-Sugiyama <>
2018-07-04 11:36:17 +0900

    [ignore]
    ignore

---
+++ Michael_Game/Assets/Resource/Script/Common/Popup/ResultOpen.cs
@@ -0,0 +1,41 @@
+﻿/**********************************************************************************************/
+/*@file       ResultOpen.cs
+*********************************************************************************************
+* @brief      リザルト用ポップアップを開くクラス
+*********************************************************************************************
+* @author     Yuta Takatsu
+*********************************************************************************************
+* Copyright © 2018 Yuta Takatsu All Rights Reserved.
+**********************************************************************************************/
+using System.Collections;
+using UnityEngine;
+
+public class ResultOpen : BaseObject {
+
+    [SerializeField]
+    private GameObject resultPopup; // @brief Resultのインスタンス化
+
+    private bool isCallOnse;
+
+    private void Start()
+    {
+        isCallOnse = false;
+    }
+
+	public void Update()
+    {
+        if (!isCallOnse)
+        {
+            if (Singleton<GameInstance>.instance.IsGoal == true)
+            {
+
+                Singleton<GameInstance>.instance.IsGoal = false;
+
+                PopupResult result = resultPopup.GetComponent<PopupResult>();
+                result.Open();
+                isCallOnse = true;
+
+            }
+        }
+    }
+}
