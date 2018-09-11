6fadf75e5f1c3b8c479462bf5c987fec0e43579a
Ryo-Sugiyama <>
2018-09-12 01:50:32 +0900

    世界の終わり

---
+++ Michael_Game/Assets/DualManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject gameObjects;
+	// Use this for initialization
+	void Start () {
+        //PhotonNetwork.isMessageQueueRunning = true;
+        GameObject gameObject = PhotonNetwork.Instantiate("Menber", new Vector3(0, 0, 0), Quaternion.Euler(Vector3.zero),0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Michael_Game/Assets/OnClickButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class OnClickButton : BaseObject {
+
+    public void OnClickSingleButton()
+    {
+        SceneManager.LoadScene("SinglePlay");
+    }
+
+    public void OnClickDualButton()
+    {
+        SceneManager.LoadScene("Matching");
+    }
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("DualPlay");
+    }
+
+}
